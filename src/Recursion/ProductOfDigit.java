5e74c2d4e535d754afc922dac0892f36408e1b3e
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-11 19:26:18 +0530

    Recusrion

---
+++ src/Recursion/ProductOfDigit.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class ProductOfDigit {
+    public static void main(String[] args) {
+        // System.out.println(sumOfDigit(13245, 0));
+        System.out.println(productOfDigit2(13245));
+    }
+
+    static int productOfDigit(int n, int sum) {
+        if (n <= 0) {
+            return sum;
+        }
+        int a = n % 10;
+        sum = sum * a;
+        int num = n / 10;
+        return productOfDigit(num, sum);
+    }
+
+    static int productOfDigit2(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n % 10 * productOfDigit2(n / 10);
+    }
+}
---
+++ src/Recursion/SumOfDigit.java
@@ -0,0 +1,31 @@
+/*
+ Sum of Digits
+Ex:- 1234
+       1+2+3+4=10
+Answer = 10
+ */
+
+package Recursion;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+       // System.out.println(sumOfDigit(13245, 0));
+        System.out.println(sumOfDigit2(13245));
+    }
+
+    static int sumOfDigit(int n, int sum) {
+        if (n<=0){
+            return sum;
+        }
+        int a = n%10;
+        sum = sum + a;
+        int num = n/10;
+        return sumOfDigit(num ,sum);
+    }
+    static int sumOfDigit2(int n) {
+        if (n==0){
+            return 0;
+        }
+        return n%10 + sumOfDigit2(n/10);
+    }
+}
