874cd917787cdac5146553a1d6dcd48b2d278f48
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-11 16:21:55 +0530

    Recusrion

--- src/Recursion/FibonaciiNumber.java
+++ src/Recursion/FibonaciiNumber.java
@@ -1,4 +1,24 @@
+// Find the Fibonacci number by recursion
+// example :- 0 1 1 2 3 5 8 13 .....n
+// formula 0 ,1  n = (n-1) + (n-2)
+
 package Recursion;
 
+import java.util.Scanner;
+
 public class FibonaciiNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(fibonocii(n));
+    }
+
+    private static int fibonocii( int n) {
+        if (n<2)
+        {
+            return n;
+        }
+        return fibonocii(n-1)+fibonocii(n-2);
+    }
 }
+// output :- It will gives the nth fibonacci number
