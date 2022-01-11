104398c1ea8772e4c3ec50e5429cb8e9111d50e9
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-12 01:16:25 +0530

    Recusrion

---
+++ src/Recursion/Array/CheckTheIsSorted.java
@@ -0,0 +1,30 @@
+package Recursion.Array;
+
+public class CheckTheIsSorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,7,8,9,223,44,44};
+        System.out.println(isSorted2(arr,0));
+    }
+    //Method 1
+    static Boolean isSorted(int[] arr, int i)
+    {
+        if (i==arr.length-1)
+        {
+            return true;
+        }
+        if (arr[i]<=arr[i+1])
+        {
+            return isSorted(arr,i+1);
+        }
+      return false;
+    }
+    //Method 2
+    static boolean isSorted2(int arr[],int n)
+    {
+        if (n==arr.length-1)
+        {
+            return true;
+        }
+        return arr[n] < arr[n+1] && isSorted2(arr,n+1);
+    }
+}
