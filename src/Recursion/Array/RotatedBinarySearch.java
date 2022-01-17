b652a63c16ab87c39c78b0bc4f597c0e2318789e
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-18 01:01:45 +0530

    Recusrion

---
+++ src/Recursion/Array/RotatedBinarySearch.java
@@ -0,0 +1,21 @@
+/*
+Input  : arr[] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+         key = 3
+Output : Found at index 8
+
+Input  : arr[] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+         key = 30
+Output : Not found
+
+Input : arr[] = {30, 40, 50, 10, 20}
+        key = 10
+Output : Found at index 3
+ */
+
+package Recursion.Array;
+
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
