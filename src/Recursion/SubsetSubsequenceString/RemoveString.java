757b66363047895cf9b7250c65cc95d4bdb900f4
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-25 22:45:20 +0530

    Recusrion

--- src/Recursion/SubsetSubsequenceString/RemoveString.java
+++ src/Recursion/SubsetSubsequenceString/RemoveString.java
@@ -1,4 +1,18 @@
 package Recursion.SubsetSubsequenceString;
 
 public class RemoveString {
+    public static void main(String[] args) {
+        System.out.println(skipString("Adityaappleadityarajsappleinghthakursahab"));
+    }
+
+    static String skipString(String str) {
+        if (str.isEmpty()) {
+            return " ";
+        }
+        if (str.startsWith("apple")) {
+            return skipString(str.substring(5));
+        } else {
+            return str.charAt(0)+skipString(str.substring(1));
+        }
+    }
 }
