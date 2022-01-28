9e808460ef4215dc2d9f562588db756d59b20fbf
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-29 00:37:54 +0530

    Recusrion

---
+++ src/Recursion/SubsetSubsequenceString/Permutation/CountNoOfPermutations.java
@@ -0,0 +1,21 @@
+package Recursion.SubsetSubsequenceString.Permutation;
+
+public class CountNoOfPermutations {
+    public static void main(String[] args) {
+        System.out.print(count("","abc"));
+    }
+
+    static int count(String processed, String unProcessed) {
+        if (unProcessed.isEmpty()) {
+            return 1;
+        }
+        int count =0 ;
+        char ch = unProcessed.charAt(0);
+        for (int i = 0; i <= processed.length(); i++) {
+            String f = processed.substring(0, i);
+            String l = processed.substring(i, processed.length());
+            count = count + count(f+ch+l,unProcessed.substring(1));
+        }
+        return count;
+    }
+}
