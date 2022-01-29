0f0328e30f1372739794ae602131d4eb7a1e2d70
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-29 15:27:01 +0530

    Recusrion

--- src/Recursion/SubsetSubsequenceString/Permutation/CompanyQuestions/DiceRollFaceGiven.java
+++ src/Recursion/SubsetSubsequenceString/Permutation/CompanyQuestions/DiceRollFaceGiven.java
@@ -1,4 +1,18 @@
 package Recursion.SubsetSubsequenceString.Permutation.CompanyQuestions;
 
 public class DiceRollFaceGiven {
+    public static void main(String[] args) {
+        dice("", 9,20);
+    }
+
+    static void dice(String pro, int target,int face) {
+        if (target == 0) {
+            System.out.println(pro);
+            return;
+        }
+
+        for (int i = 1; i <= face && i <= target; i++) {
+            dice(pro + i, target - i,face);
+        }
+    }
 }
