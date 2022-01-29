161f2b4cb8d2a67ff2c6cc6b1f308af7e95a8342
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-29 19:45:16 +0530

    Backtracking

--- src/Backtracking/MazeOnlyRightAndDownSteps.java
+++ src/Backtracking/MazeOnlyRightAndDownSteps.java
@@ -1,24 +1,28 @@
 package Backtracking;
 
+import java.util.ArrayList;
+
 /*
 Source = (3,3)
 Destination = (1.1)
  */
 public class MazeOnlyRightAndDownSteps {
     public static void main(String[] args) {
-        System.out.println(mazeCountPath(4,4));
-        mazePrintPath(4,4,"");
+        mazePrintPath2(3, 3, "");
+        System.out.println(mazeCountPath(3, 3));
+        mazePrintPath(3, 3, "");
+        System.out.println(mazePathReturnInArraylist(3, 3, ""));
     }
 
     static int mazeCountPath(int row, int col) {
+        if (row == 1 && col == 1) { // when it will reach to index(1,1)
+            return 1;
+        }
         if (row < 1) { //checking row index so that it can't cross the matrix
             return 0;
         }
         if (col < 1) { //checking column index so that it can't cross the matrix
             return 0;
-        }
-        if (row == 1 && col == 1) { // when it will reach to index(1,1)
-            return 1;
         }
         int left = mazeCountPath(row - 1, col);
         int right = mazeCountPath(row, col - 1);
@@ -26,18 +30,50 @@ public class MazeOnlyRightAndDownSteps {
     }
 
     static void mazePrintPath(int row, int col, String processed) {
+        if (row == 1 && col == 1) { // when it will reach to index(1,1)
+            System.out.println(processed);
+            return;
+        }
         if (row < 1) { //checking row index so that it can't cross the matrix
             return;
         }
         if (col < 1) { //checking column index so that it can't cross the matrix
             return;
         }
+        mazePrintPath(row - 1, col, processed + 'd');//down step
+        mazePrintPath(row, col - 1, processed + "r");//right step
+
+    }
+
+
+    static void mazePrintPath2(int row, int col, String processed) {
         if (row == 1 && col == 1) { // when it will reach to index(1,1)
             System.out.println(processed);
             return;
         }
-        mazePrintPath(row - 1, col,processed + 'd' );//down step
-        mazePrintPath(row, col - 1,processed + "r" );//right step
+        if (row > 1) { //checking row index so that it can't cross the matrix
+            mazePrintPath2(row - 1, col, processed + 'd');//down step
+        }
+        if (col > 1) { //checking column index so that it can't cross the matrix
+            mazePrintPath2(row, col - 1, processed + "r");//right step
+        }
+    }
 
+    static ArrayList<String> mazePathReturnInArraylist(int row, int col, String processed) {
+        if (row == 1 && col == 1) { // when it will reach to index(1,1)
+            ArrayList<String> s = new ArrayList<>();
+            s.add(processed);
+            return s;
+        }
+        ArrayList<String> st = new ArrayList<>();
+        if (row > 1) { //checking row index so that it can't cross the matrix
+            ArrayList<String> d = mazePathReturnInArraylist(row - 1, col, processed + 'd');//down step
+            st.addAll(d);
+        }
+        if (col > 1) { //checking column index so that it can't cross the matrix
+            ArrayList<String> r = mazePathReturnInArraylist(row, col - 1, processed + "r");//right step
+            st.addAll(r);
+        }
+        return st;
     }
 }
