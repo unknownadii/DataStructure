a98595899b5343d616b01d545b52fd7e64d7f102
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-01-31 22:28:34 +0530

    Assignment

--- src/Backtracking/Assignment/Easy/PowerOfTwo.java
+++ src/Backtracking/Assignment/Easy/PowerOfTwo.java
@@ -1,4 +1,29 @@
 package Backtracking.Assignment.Easy;
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
 
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+ */
 public class PowerOfTwo {
+    public static void main(String[] args) {
+
+    }
+    static boolean isPowerOfTwo(int n) {
+
+        return false;
+    }
 }
