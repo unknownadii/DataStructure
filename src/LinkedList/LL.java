1ccf9a141c7148c24236c4bd503ff6f954e8f78a
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-02-02 16:37:31 +0530

    LinkedList

---
+++ src/LinkedList/LL.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp=temp.next;
+        }
+        System.out.println("END");
+    }
+
+    class Node {
+        private int val;
+        private Node next;
+
+        public Node(int value) {
+            this.val = value;
+        }
+
+        public Node(int value, Node next) {
+            this.val = value;
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/LinkedList/Main.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(33);
+        list.insertFirst(12);
+        list.insertFirst(21);
+        list.insertFirst(56);
+        list.display();
+    }
+}
