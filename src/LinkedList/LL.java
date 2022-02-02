20b11da15ea14784b576dc0c1b75b266092a597d
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-02-02 17:43:10 +0530

    LinkedList

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -4,7 +4,7 @@ public class LL {
 
     private Node head;
     private Node tail;
-    private int size;
+    public int size;
 
     public LL() {
         this.size = 0;
@@ -17,15 +17,65 @@ public class LL {
         if (tail == null) {
             tail = head;
         }
+        size++;
     }
 
     public void display() {
         Node temp = head;
         while (temp != null) {
             System.out.print(temp.val + " -> ");
-            temp=temp.next;
+            temp = temp.next;
         }
         System.out.println("END");
+    }
+
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node temp = new Node(val);
+        tail.next = temp;
+        tail = temp;
+        size++;
+    }
+
+    public void insertLastByHead(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            insertFirst(val);
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+        size++;
+    }
+
+    public void insert(int val, int index) {
+        Node node = new Node(val);
+        if (index == 0 || head == null) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+        if (index > size) {
+            System.out.println("Out of Range Index");
+            return;
+        }
+        Node temp = head;//already at 0 index so starting "i" from 1
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        Node helper = temp.next;
+        temp.next = node;
+        node.next = helper;
+        size++;
     }
 
     class Node {
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -8,5 +8,12 @@ public class Main {
         list.insertFirst(21);
         list.insertFirst(56);
         list.display();
+        list.insertLast(99);
+        list.display();
+        list.insertLastByHead(100);
+        list.display();
+        list.insert(22,100);
+        list.insert(22,3);
+        list.display();
     }
 }
