9e2aa92fab13e1494d831b1d8b7a64619798e26f
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-02-05 01:03:54 +0530

    LinkedList

---
+++ src/LinkedList/Questions/HappyNumberGoogle.java
@@ -0,0 +1,70 @@
+package LinkedList.Questions;
+
+/*
+202. Happy Number
+Easy
+
+4584
+
+641
+
+Add to List
+
+Share
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ */
+public class HappyNumberGoogle {
+    public static void main(String[] args) {
+        System.out.println(isHappy(2));
+    }
+
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            //one steps at a time for 1st pointer
+            slow = findSquare(slow);
+            //two steps at time for faster/second pointer
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);//when slow meets the fast pointer then it's confirmed that it is cyclic so end the loop
+
+        // check for happy number
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int findSquare(int num) {
+        int ans = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            ans += rem * rem;
+            num = num / 10;
+        }
+        return ans;
+    }
+}
