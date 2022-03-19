04a07bfb858b15764cb57d7d3630cb8906cc5d99
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-03-19 13:26:54 +0530

    HashMap

---
+++ src/HashMap/CountTheFrequencyOfCharacterInString.java
@@ -0,0 +1,30 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CountTheFrequencyOfCharacterInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any String");
+        String str = sc.next();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        Set<Character> set = map.keySet();
+        Character chr = str.charAt(0);
+        for (Character c : set) {
+            if (map.get(c) > map.get(chr)) {
+                chr = c;
+            }
+        }
+        System.out.println(chr);
+    }
+}
---
+++ src/HashMap/HashMapPreBuild.java
@@ -0,0 +1,29 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashMapPreBuild {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Aditya", 20);
+        map.put("Rahul", 22);
+        map.put("Rohit", 23);
+        map.put("Arjun", 24);
+        map.put("Krishna", 26);
+        map.put("Ram", 21);
+        System.out.println(map);
+        map.remove("Rahul");
+        System.out.println(map);
+        System.out.println(map.get("Aditya"));
+        System.out.println(map.containsKey("Aditya"));
+
+        Set<String> keySet = map.keySet();
+        System.out.println(keySet);
+
+        for (String key : keySet) {
+            Integer val = map.get(key);
+            System.out.println(key + " " + val);
+        }
+    }
+}
