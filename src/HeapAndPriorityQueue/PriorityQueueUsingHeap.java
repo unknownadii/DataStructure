454569c20a34fd9adb18ef58faca3c5f62872e79
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-03-21 00:45:59 +0530

    Heap And Priority Queue

--- src/HeapAndPriorityQueue/PriorityQueueUsingHeap.java
+++ src/HeapAndPriorityQueue/PriorityQueueUsingHeap.java
@@ -11,9 +11,24 @@ public class PriorityQueueUsingHeap {
     }
 
     //Add Function
+    // (logn)-> for 1 element adding
+    // nlogn -> for n elements it will takes 0(nlogn) time complexity
     public void add(int val) {
         data.add(val);
         upHeapify(data.size() - 1);
+    }
+
+    // Add Function
+    // (o(n)) time complexity -> for n element addition
+    public void addLinearly(int arr[]) {
+        data = new ArrayList<>();
+        // add all value firstly
+        for (int val : arr) {
+            data.add(val);
+        }
+        for (int i = (data.size() / 2) - 1; i >= 0; i--) {
+            downHeapify(i);
+        }
     }
 
     private void upHeapify(int childIndex) {
@@ -28,35 +43,38 @@ public class PriorityQueueUsingHeap {
     }
 
     private void swapIndex(int parentIndex, int childIndex) {
-        int temp = data.get(parentIndex);
-        data.set(parentIndex, data.get(childIndex));
-        data.set(childIndex, temp);
+        int parent = data.get(parentIndex);
+        int child = data.get(childIndex);
+
+        data.set(parentIndex, child);
+        data.set(childIndex, parent);
     }
 
     //Remove Function
-    public void removeValue() {
-        if (data.size() == 0) {
+    public int removeValue() {
+        if (this.size() == 0) {
             System.out.println("UnderFlow Condition");
         }
         //Step 1:- swap 1st and last index to maintain the logn complexity while removing.
         swapIndex(0, data.size() - 1);
-        System.out.println(data.get(data.size() - 1));
         // Step 2 :- Remove the last index value.
-        data.remove(data.size() - 1);
+        int val = data.remove(data.size() - 1);
         downHeapify(0);
+        return val;
     }
 
     private void downHeapify(int parentIndex) {
         int temp = parentIndex;
         int leftChildIndex = 2 * parentIndex + 1;
         //checking left index with parent
-        if (leftChildIndex < data.size() && data.get(leftChildIndex) < data.get(parentIndex)) {
+        if (leftChildIndex < data.size() && data.get(leftChildIndex) < data.get(temp)) {
             temp = leftChildIndex;
         }
         // checking left with right if above condition is true
         // if false then checking it with parent
+        // and temp variable will store it
         int rightChildIndex = 2 * parentIndex + 2;
-        if (rightChildIndex < data.size() && data.get(rightChildIndex) < data.get(parentIndex)) {
+        if (rightChildIndex < data.size() && data.get(rightChildIndex) < data.get(temp)) {
             temp = rightChildIndex;
         }
         //temp!=parentIndex if this condition is false that means the parent have high priority than child
@@ -64,11 +82,12 @@ public class PriorityQueueUsingHeap {
             swapIndex(parentIndex, temp);
             downHeapify(temp);
         }
+
     }
 
     //peek
     public int peek() {
-        if (data.size() == 0) {
+        if (this.size() == 0) {
             System.out.println("underflow condition arises");
             return -1;
         }
@@ -85,23 +104,32 @@ public class PriorityQueueUsingHeap {
 class Main {
     public static void main(String[] args) {
         PriorityQueueUsingHeap obj = new PriorityQueueUsingHeap();
-        //add
-        obj.add(2);
-        obj.add(3);
-        obj.add(6);
-        obj.add(1);
-        obj.add(7);
-        obj.add(9);
-        obj.add(8);
+        //add nlogn
+        int arr[] = {1, 2, 4, 3, 5, 7, 4, 6, 34, 8, 0};
+        int arr2[] = {12, 25, 44, 37, 215, 57, 49, 65, 34, 18, 60};
+        for (int i = 0; i < arr.length; i++) {
+            obj.add(arr[i]);
+        }
         //display
-        obj.removeValue();
-        obj.removeValue();
-        obj.removeValue();
-        obj.removeValue();
-        obj.removeValue();
+        int size = obj.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(obj.removeValue());
+            System.out.println("Size => " + obj.size());
+        }
 
-        // peek
-        System.out.println("Peek :- "+obj.peek());
+        System.out.println();
+        // adding 0(n)
+        obj.addLinearly(arr2);
+
+        System.out.println("Size => " + obj.size());
+        //display
+        int sizelinear = obj.size();
+        for (int i = 0; i < sizelinear; i++) {
+            System.out.println(obj.removeValue());
+            System.out.println("Size => " + obj.size());
+        }
+        System.out.println("Size => " + obj.size());
+
 
     }
 }
