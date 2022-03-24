ee8109feadbe9754e565986eb8e784ae4abfc387
unknownadii <aditya.singh_cs19@gla.ac.in>
2022-03-25 00:17:19 +0530

    Graph

---
+++ src/Graph/Questions/GetAllConnectedComponentOfAGraph.java
@@ -0,0 +1,46 @@
+package Graph.Questions;
+
+import Graph.ConstructGraph;
+
+import java.util.ArrayList;
+
+public class GetAllConnectedComponentOfAGraph {
+
+    public static void getConnectedComponent(ArrayList<ConstructGraph.Edges>[] graph, boolean[] visited,int vertex) {
+        ArrayList<ArrayList<Integer>> allComponents = new ArrayList<>();
+        for (int i = 0; i < vertex; i++) {
+            if (!visited[i]) {
+                ArrayList<Integer> connectedNodes = new ArrayList<>();
+                getConnectedComponentHelper(graph, i, visited, connectedNodes);
+                allComponents.add(connectedNodes);
+            }
+        }
+        System.out.println(allComponents);
+    }
+
+    private static void getConnectedComponentHelper(ArrayList<ConstructGraph.Edges>[] graph, int src,
+                                                    boolean[] visited, ArrayList<Integer> connectedNodes) {
+        connectedNodes.add(src);
+        visited[src] = true;
+        for (ConstructGraph.Edges edges : graph[src]) {
+            if (!visited[edges.neighbour]) {
+                getConnectedComponentHelper(graph, edges.neighbour, visited, connectedNodes);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ConstructGraph obj = new ConstructGraph(7);
+        System.out.println("Weighted UnDirected Graph");
+        obj.constructUnDirectedGraph(0, 1, 10);
+        obj.constructUnDirectedGraph(1, 2, 10);
+        obj.constructUnDirectedGraph(3, 4, 10);
+        obj.constructUnDirectedGraph(6, 5, 10);
+        obj.display();
+        System.out.println();
+        // we are using visited to avoid the condition of cyclic graph in which it will rotate infinitely
+        int numberOfNodes = 7;
+        boolean[] visited = new boolean[numberOfNodes];
+         getConnectedComponent(obj.graph, visited,7);
+    }
+}
